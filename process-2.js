17ec347c5407d6eaf85d14b7041e67092725f348
Vansham Aggarwal <vanshamagg@gmail.com>
2021-03-14 21:04:50 +0530

    code added

--- process-2.js
+++ process-2.js
@@ -1,4 +1,4 @@
-const FLAG = 1;
+const FLAG = 0;
 
 if (FLAG === 1) {
     console.log("ABORT MISSION");
@@ -6,3 +6,13 @@ if (FLAG === 1) {
 }
 
 console.log("THIS IS NICE");
+
+console.log(process.allowedNodeEnvironmentFlags.has('report-directory'))
+
+console.log("PROCESS ARCH = " + process.arch)
+
+console.log("ARGUMENTS FROM THE COMMAND LINE", process.argv)
+
+console.log('current dir -> ', process.cwd())
+
+process.chdir('/home/vansham/SANCTUM/MISC-PROJECTS/practice/another')
