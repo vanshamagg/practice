ba1b7e9f5320a927bd28dfc37a60e8ab6b080954
Vansham Aggarwal <vanshamagg@gmail.com>
2021-03-14 20:31:49 +0530

    code added

--- process.js
+++ process.js
@@ -7,3 +7,9 @@ process.on("beforeExit", (code) => {
 process.on("exit", (code) => {
     console.log("this is where the code exits completely with code " + code);
 });
+
+process.emitWarning('Hey Dont do that', 'warning1')
+
+process.on('warning', (warning)=> {
+    console.log(`A warning was emitted \n ${warning}`)
+})
