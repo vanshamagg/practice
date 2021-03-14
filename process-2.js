28e8d1e327ec4ede22fc23a40b87525462b10230
Vansham Aggarwal <vanshamagg@gmail.com>
2021-03-14 21:40:20 +0530

    code added

--- process-2.js
+++ process-2.js
@@ -16,3 +16,17 @@ console.log("ARGUMENTS FROM THE COMMAND LINE", process.argv)
 console.log('current dir -> ', process.cwd())
 
 console.log('config options -> ', process.config)
+
+console.log('CPU USAGE -> ', process.cpuUsage())
+
+console.log("HIGH RESOLUTION TIME -> ", process.hrtime())
+
+console.log("MEMORY USAGE ->", process.memoryUsage())
+
+console.log("PROCESS ID -> ", process.pid)
+
+console.log("CURRENT PLATFORM ->", process.platform)
+
+console.log("RELEASE INFO ->", process.release)
+
+console.log('REPORT ->', process.report)
