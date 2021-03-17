90b039c592648b97d42735cd1b72166dc85bda59
Vansham Aggarwal <vanshamagg@gmail.com>
2021-03-17 22:49:57 +0530

    practice

--- streams.ts
+++ streams.ts
@@ -1,6 +1,6 @@
 import { createReadStream } from 'fs';
 
 
-createReadStream('some-file.json', { encoding: 'utf-8' }).on('data', (chunk) => {
-    console.log(chunk + "\n")
+createReadStream('some-file.json', { encoding: 'utf-8', highWaterMark: 1 }).on('data', (chunk) => {
+    process.stdout.write(chunk + '\n')
 })
