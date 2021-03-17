18065dcedc6abdc3c32a7c396a51c8e937217a8b
Vansham Aggarwal <vanshamagg@gmail.com>
2021-03-17 23:07:33 +0530

    practice

--- streams.ts
+++ streams.ts
@@ -1,6 +1,25 @@
 import { createReadStream } from 'fs';
+import { read } from 'fs/promises';
 
 
-createReadStream('some-file.json', { encoding: 'utf-8', highWaterMark: 1 }).on('data', (chunk) => {
+const readable = createReadStream('some-file.json', { encoding: 'utf-8', highWaterMark: })
+
+readable.on('data', (chunk) => {
+    process.stdout.cork()
+
     process.stdout.write(chunk + '\n')
+    
+
 })
+
+readable.on('close', ()=> process.stdout.uncork())
+
+// setTimeout(() => { process.stdout.uncork() }, 0)
+
+process.stdout.cork()
+process.stdout.write("SOMETHING \n")
+process.stdout.cork()
+process.stdout.write("ELSE \n")
+
+process.stdout.uncork()
+// process.stdout.uncork()
