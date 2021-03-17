8d088859b824ad5640073b7567331953fcc0bdf4
Vansham Aggarwal <vanshamagg@gmail.com>
2021-03-17 23:13:58 +0530

    practice

--- streams.ts
+++ streams.ts
@@ -2,24 +2,30 @@ import { createReadStream } from 'fs';
 import { read } from 'fs/promises';
 
 
-const readable = createReadStream('some-file.json', { encoding: 'utf-8', highWaterMark: })
+const readable = createReadStream('some-file.json', { encoding: 'utf-8' })
 
-readable.on('data', (chunk) => {
-    process.stdout.cork()
+// readable.on('data', (chunk) => {
 
-    process.stdout.write(chunk + '\n')
-    
+//     process.stdout.write(chunk + '\n')
 
-})
+//     console.log("BYTES READY ---",process.stdout.writableLength)
 
-readable.on('close', ()=> process.stdout.uncork())
+// })
 
-// setTimeout(() => { process.stdout.uncork() }, 0)
+readable.pipe(process.stdout)
 
 process.stdout.cork()
 process.stdout.write("SOMETHING \n")
 process.stdout.cork()
 process.stdout.write("ELSE \n")
+console.log(process.stdout.writableCorked)
+
 
 process.stdout.uncork()
-// process.stdout.uncork()
+process.stdout.uncork()
+
+console.log("--- WRITABLE STREAM PROPS ---")
+console.log(process.stdout.writableHighWaterMark)
+console.log(process.stdout.writableFinished)
+console.log(process.stdout.writableCorked)
+console.log(process.stdout.writableLength)
