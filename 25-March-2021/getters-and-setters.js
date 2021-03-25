897d04a9cbed7bf25633cf2d26d3cd6097917b4d
Vansham Aggarwal <vanshamagg@gmail.com>
2021-03-26 00:25:22 +0530

    getters setters in JS

---
+++ 25-March-2021/getters-and-setters.js
@@ -0,0 +1,15 @@
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+
+    get details() {
+        return this.name;
+    }
+
+    set details(val){
+        this.address = val 
+    }
+}
+
+
