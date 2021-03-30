368da1d6c45e7ef21eb65ce046d90770a033aefd
Vansham Aggarwal <vanshamagg@gmail.com>
2021-03-30 22:27:35 +0530

    bull pckage

---
+++ 30-March-2021/bull.ts
@@ -0,0 +1,32 @@
+import * as Bull from 'bull'
+
+const myFirstQueue = new Bull('my-first-queue');
+
+
+(async ()=> {
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    await myFirstQueue.add({ name: "Vansham" })
+    process.exit(0)
+    return `completed`
+})()
