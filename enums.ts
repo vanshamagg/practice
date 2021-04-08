0a6fca49a037a66fd07896336b9f4206e0ad1e35
Vansham Aggarwal <vanshamagg@gmail.com>
2021-04-08 19:06:20 +0530

    enumsg

---
+++ enums.ts
@@ -0,0 +1,18 @@
+enum Numeric {
+    ONE,
+    TWO,
+    THREE
+}
+
+enum Alpha {
+    ONE = "one",
+    TWO = "two",
+    THREE = "three"
+}
+
+
+let movieType: Numeric
+
+movieType =  Numeric[""]
+
+Alpha[""]
