dad6ee0fe08f76ed5663fafba235a1e7923698ad
Vansham Aggarwal <vanshamagg@gmail.com>
2021-05-29 18:24:57 +0530

    ARRAYS AND VECTORS

--- c++/std::array.cpp
+++ c++/std::array.cpp
@@ -6,6 +6,7 @@ int main()
 
     std::array<int, 5> numbers{1, 2, 3, 4, 5};
 
+    std::cout << numbers.at(4) << std::endl;
 
     return 0;
 }
--- node/readline.js
+++ node/readline.js
@@ -1,29 +1,29 @@
-const { clearLine, cursorTo, moveCursor } = require( 'readline' );
+const { clearLine, cursorTo } = require('readline');
 
 console.log();
-function printProgress( total, progress, x, message ) {
+function printProgress(total, progress, x, message) {
 
     const BAR_SIZE = 70;
-    const progressBar = new Array( BAR_SIZE ).fill( ' ' );
-    const progressPoints = Math.floor( ( progress * BAR_SIZE ) / total );
+    const progressBar = new Array(BAR_SIZE).fill(' ');
+    const progressPoints = Math.floor((progress * BAR_SIZE) / total);
 
-    for ( let i = 0; i < progressPoints; i++ )
+    for (let i = 0; i < progressPoints; i++)
         progressBar[i] = '\u2588';
 
-    cursorTo( process.stdout, x );
-    clearLine( process.stdout, -1 );
-    clearLine( process.stdout, 1 );
-    process.stdout.write( ' [ ' + progressBar.join( '' ) + ' ] ' + Math.round( progress / total * 10000 ) / 100 + '% ' + message );
+    cursorTo(process.stdout, x);
+    clearLine(process.stdout, -1);
+    clearLine(process.stdout, 1);
+    process.stdout.write(' [ ' + progressBar.join('') + ' ] ' + Math.round(progress / total * 10000) / 100 + '% ' + message);
 
 }
 
-// let i = 0;
+let i = 0;
 
-// setInterval( () => {
+setInterval( () => {
 
-//     const message = `(${ i }/100)`;
-//     printProgress( 130, i, 0, message );
-//     i++;
+    const message = `(${ i }/100)`;
+    printProgress( 130, i, 0, message );
+    i++;
 
-// }, 500 );
+}, 500 );
 
