0159d3e38fbdf3ee9d7989457dca6afec3f15850
Vansham Aggarwal <vanshamagg@gmail.com>
2021-07-15 21:52:47 +0530

    PATH MODULE

--- node/net/client.js
+++ node/net/client.js
@@ -1,9 +1,10 @@
 const { createConnection } = require('net');
 
 const socket = createConnection(3000, 'localhost', () => {
-    console.info('CONNECTED TO SERVER')
-    setInterval(() => {
-       socket.write(`Vansham (${new Date().toLocaleString()})`) 
-    }, 1000);
-    socket.on('close', ()=> console.log('DISCONNECTED FROM SERVER'))
+    console.info('CONNECTED TO SERVER');
+    // setInterval(() => {
+    //    socket.write(`Vansham (${new Date().toLocaleString()})`) 
+    // }, 1000);
+    socket.setTimeout(1000, () => console.info('Socket has timed out'));
+    socket.on('close', () => console.log('DISCONNECTED FROM SERVER'));
 });
---
+++ node/path/main.js
@@ -0,0 +1,4 @@
+const path = require('path');
+
+path.resolve('/', 'foo', 'bar', '..')
+path.join('foo', 'bar', 'baz', 'quux')
