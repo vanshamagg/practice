d6513144d0fdd6b81243943197facf0427a91382
Vansham Aggarwal <vanshamagg@gmail.com>
2021-09-08 16:55:52 +0530

    decorators

--- typescript/reflection.ts
+++ typescript/reflection.ts
@@ -1,28 +1,54 @@
+import { Method } from 'axios';
+import { METHODS } from 'http';
 import 'reflect-metadata';
 
-// Class Decorator
-function SetMetadata(): ClassDecorator {
-    return (target) => {
-        console.log('Decorator Activated');
-        Reflect.defineMetadata('random', 'value', target)
-    };
-}
+const TOPIC = 'topic:name';
+const TOPIC_HANDLERS = 'topic:handlers:hash';
 
-// Parameter Decorator
-function PMetadata(): ParameterDecorator {
-    return (target: object, key, index) => {
-        console.log(target)
+// topic decorator
+function Topic(topic: string): any {
+  return (target: Object, methodName, descriptor: TypedPropertyDescriptor<any>): void => {
+    Reflect.defineMetadata(TOPIC, topic, target, methodName);
+    // return descriptor;
+  };
+};
+
+// class decorator
+function Multiplexer(): ClassDecorator {
+  return (target) => {
+    const hash = {};
+    Reflect.defineMetadata(METHODS, Object.keys(target.prototype), target.prototype);
+    Object.keys(target.prototype)
+      .filter(key => Reflect.hasOwnMetadata(TOPIC, target.prototype, key))
+      .forEach(key => hash[Reflect.getMetadata(TOPIC, target.prototype, key)] = target.prototype[key]);
+
+    Reflect.defineMetadata(TOPIC_HANDLERS, hash, target.prototype);
+  };
+};
+
+@Multiplexer()
+class TopicHandler {
+
+  @Topic('vansham')
+  public handleVansham(data: any) {
+    console.log('DATA RECEIVED', data);
+  }
+
+  @Topic('shweta')
+  public handle(data: any): any {
+    console.log('DATA RECEIVED', data);
+
+  }
+
+  multiplexData(data: any) {
+    const topic = data['topic'];
+    if (topic) {
+      Reflect.getMetadata(TOPIC_HANDLERS, TopicHandler.prototype)[topic](data)
     }
-}
-@SetMetadata()
-class Person {
-    name: string = "Vansham";
+  }
 
-    getName(@PMetadata() name?: string) {
-        console.log(name)
-    }
 }
 
-
-const p1 = new Person()
-p1.getName()
+const th = new TopicHandler();
+console.info(Reflect.getOwnMetadata(TOPIC_HANDLERS, TopicHandler.prototype));
+th.multiplexData({ topic: 'vansham' });
